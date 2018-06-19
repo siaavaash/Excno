0cd1493edb41469766f0615361fc2b1c047e6ef7
Siavash Safi <theexxo@hotmail.com>
2018-06-19 10:26:46 +0430

    Add project files.

---
+++ Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+namespace Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Excno/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Excno.MvcApplication" Language="C#" %>
